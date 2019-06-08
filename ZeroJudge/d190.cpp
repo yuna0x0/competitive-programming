ad1939675e4e179188e1bdfe6efa08c94cbd786a
Edison Lee <edisonlee@edisonlee55.com>
2019-06-08 22:47:29 +0800

    Add ZeroJudge d190.cpp

---
+++ ZeroJudge/d190.cpp
@@ -0,0 +1,28 @@
+/*
+ * UVa Q11462: Age Sort
+ * ZeroJudge d190
+ * Lucky Cat Link: http://luckycat.kshs.kh.edu.tw/homework/q11462.htm
+ */
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int n;
+    int arr[2000005];
+    while (cin >> n)
+    {
+        if (n == 0)
+            break;
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+        sort(arr, arr + n);
+        for (int i = 0; i < n; i++)
+            if (i != n - 1)
+                cout << arr[i] << " ";
+            else
+                cout << arr[i];
+        cout << endl;
+    }
+    return 0;
+}
