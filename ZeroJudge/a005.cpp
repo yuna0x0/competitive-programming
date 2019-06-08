cf5886c775b8104226d39fe633d84e246dda0da1
Edison Lee <edisonlee@edisonlee55.com>
2019-06-08 08:56:15 +0800

    Add ZeroJudge a005.cpp

---
+++ ZeroJudge/a005.cpp
@@ -0,0 +1,36 @@
+/*
+ * ZeroJudge a005
+ * Tag: Loop
+ */
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t = 0;
+    int arr[10] = {0};
+    cin >> t;
+    while (t--)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            cin >> arr[i];
+        }
+        if (arr[1] - arr[0] == arr[2] - arr[1])
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                cout << arr[i] << " ";
+            }
+            cout << arr[3] + (arr[1] - arr[0]) << endl;
+        }
+        else
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                cout << arr[i] << " ";
+            }
+            cout << arr[3] * (arr[1] / arr[0]) << endl;
+        }
+    }
+    return 0;
+}
