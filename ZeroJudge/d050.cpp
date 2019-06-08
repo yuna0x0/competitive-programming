5d5225522b2d564f58179915e48020181b8fecc9
Edison Lee <edisonlee@edisonlee55.com>
2019-06-08 21:57:31 +0800

    Add ZeroJudge d050.cpp

---
+++ ZeroJudge/d050.cpp
@@ -0,0 +1,15 @@
+/*
+ * ZeroJudge d050
+ */
+#include <iostream>
+using namespace std;
+int main()
+{
+    int h = 0;
+    cin >> h;
+    if (h - 15 < 0)
+        cout << 24 + (h - 15) << endl;
+    else
+        cout << h - 15 << endl;
+    return 0;
+}
