b493342f8a24c6884391af547e95be750665abd0
Edison Lee <edisonlee@edisonlee55.com>
2019-06-09 23:11:30 +0800

    Add ZeroJudge d583.cpp

---
+++ ZeroJudge/d583.cpp
@@ -0,0 +1,26 @@
+/*
+ * ZeroJudge d583 (C++)
+ * Link: https://zerojudge.tw/ShowProblem?problemid=d583
+ */
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int n = 0;
+    int arr[100005];
+    while (cin >> n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        sort(arr, arr + n);
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
