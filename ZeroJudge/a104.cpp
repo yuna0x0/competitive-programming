21840389bafc5b6c8489f57e8c8ee830addde503
Edison Lee <edisonlee@edisonlee55.com>
2019-06-09 23:05:12 +0800

    Add ZeroJudge a104.cpp

---
+++ ZeroJudge/a104.cpp
@@ -0,0 +1,26 @@
+/*
+ * ZeroJudge a104
+ * Link: https://zerojudge.tw/ShowProblem?problemid=a104
+ */
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int n = 0;
+    int arr[1005];
+    while (cin >> n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        sort(arr, arr + n);
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
