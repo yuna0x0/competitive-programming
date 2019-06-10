8909b922e6b56055e6fc489a20add855d708175f
Edison Lee <edisonlee@edisonlee55.com>
2019-06-10 19:41:04 +0800

    Add ZeroJudge d049.cpp

---
+++ ZeroJudge/d049.cpp
@@ -0,0 +1,14 @@
+/*
+ * ZeroJudge d049 (C++)
+ * Link: https://zerojudge.tw/ShowProblem?problemid=d049
+ */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int year = 0;
+    cin >> year;
+    cout << year - 1911 << endl;
+    return 0;
+}
