d4806d6577ad31d4075f074cc501acd741c56c60
Edison Lee <edisonlee@edisonlee55.com>
2019-06-10 20:42:19 +0800

    Add ZeroJudge d064.cpp

---
+++ ZeroJudge/d064.cpp
@@ -0,0 +1,17 @@
+/*
+ * ZeroJudge d064 (C++)
+ * Link: https://zerojudge.tw/ShowProblem?problemid=d064
+ */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 0;
+    cin >> i;
+    if (i % 2 == 0)
+        cout << "Even" << endl;
+    else
+        cout << "Odd" << endl;
+    return 0;
+}
