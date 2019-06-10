b72df9fbb613ad21067cb1c5c455b4efbdeb54f8
Edison Lee <edisonlee@edisonlee55.com>
2019-06-10 19:38:40 +0800

    Add ZeroJudge a001.cpp

---
+++ ZeroJudge/a001.cpp
@@ -0,0 +1,16 @@
+/*
+ * ZeroJudge a001 (C++)
+ * Link: https://zerojudge.tw/ShowProblem?problemid=a001
+ */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str;
+    while (cin >> str)
+    {
+        cout << "hello, " << str << endl;
+    }
+    return 0;
+}
