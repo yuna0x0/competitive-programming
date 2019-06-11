1c174f29fe10fe1448f313a27dfb6f11ec7af50e
Edison Lee <edisonlee@edisonlee55.com>
2019-06-11 21:20:52 +0800

    Update ZeroJudge d498.cpp

---
+++ ZeroJudge/d498.cpp
@@ -0,0 +1,15 @@
+/*
+ * ZeroJudge d498 (C++)
+ * Link: https://zerojudge.tw/ShowProblem?problemid=d498
+ */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cout << "I don't say swear words!" << endl;
+    return 0;
+}
