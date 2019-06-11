d8c741dd472ebd0d2b180d1695795e2d01afb95a
Edison Lee <edisonlee@edisonlee55.com>
2019-06-11 13:12:15 +0800

    Add ZeroJudge a012.cpp

---
+++ UVa/10055.cpp
@@ -0,0 +1,18 @@
+/*
+ * UVa Q10055: Hashmat the brave warrior (C++)
+ * ZeroJudge a012
+ * Lucky Cat Link: http://luckycat.kshs.kh.edu.tw/homework/q10055.htm
+ * ZeroJudge Link: Link: https://zerojudge.tw/ShowProblem?problemid=a012
+ */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long a, b;
+    while (cin >> a >> b)
+    {
+        cout << abs(a - b) << endl;
+    }
+    return 0;
+}
